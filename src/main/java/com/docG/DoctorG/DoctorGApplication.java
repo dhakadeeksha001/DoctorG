453924138ba5c6ece8f4ebc6aa9712e37d5863e9
Deeksha Dhakad <dhakadeeksha001@gmail.com>
2026-06-22 11:03:54 +0530

    Initial project setup

---
+++ src/main/java/com/docG/DoctorG/DoctorGApplication.java
@@ -0,0 +1,13 @@
+package com.docG.DoctorG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorGApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docG/DoctorG/DoctorGApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docG.DoctorG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorGApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
